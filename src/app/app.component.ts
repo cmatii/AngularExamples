772cc81cb9399ee1533c74ca63ffdb9316c83f0b
cmatii <matias.vargas.marin@gmail.com>
2024-09-11 21:40:46 -0300

    Adding homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MyStandaloneComponentComponent } from './my-standalone-component/my-standalone-component.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HelloWorldComponent, MyStandaloneComponentComponent, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, HelloWorldComponent, MyStandaloneComponentComponent, RouterLink, RouterLinkActive, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { MyStandaloneComponentComponent } from './my-standalone-component/my-standalone-component.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { HomeComponent } from './home/home.component';
+import { Component } from '@angular/core';
 
 
 export const routes: Routes = [
     { path: 'hellorWorld-component', component: HelloWorldComponent },
-    { path: 'mystandAlones-component', component: MyStandaloneComponentComponent}
+    { path: 'mystandAlones-component', component: MyStandaloneComponentComponent},
+    { path: 'home-component', component: HomeComponent}
 ];
 
 
